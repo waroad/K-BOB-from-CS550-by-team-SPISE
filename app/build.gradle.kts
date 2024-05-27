3df34b2c55c1116941896dfb2d258c9dadcaf9f0
hyunsunhong <gustjs5419@naver.com>
2024-05-27 13:41:47 +0900

    인석 + 현선 통합

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("com.google.code.gson:gson:2.10.1")
 }
